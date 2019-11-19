fd67e498696ff762c3c2ab8509914bddea0a5c06
giorgikhachidze <xacho.95@gmail.com>
2019-11-19 20:55:28 +0400

    the collate is added to migration.

--- migrations/2019-11-16-000655_create_users_sessions/up.sql
+++ migrations/2019-11-16-000655_create_users_sessions/up.sql
@@ -4,4 +4,4 @@ CREATE TABLE `users_sessions` (
   `user_id` bigint(20) unsigned NOT NULL,
   `token` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
